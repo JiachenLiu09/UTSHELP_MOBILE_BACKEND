85eb3916648b47be1ab9ae23bc1c334aa717f5bc
jiachen <liujiachen9702@gmail.com>
2019-08-01 14:17:56 +1000

    Change the port from 8081 to 8888

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.post('/login', function (req, res) {
     })
  })
 
-var server = app.listen(8081, function () {
+var server = app.listen(8888, function () {
  
   var host = server.address().address
   var port = server.address().port
