2f4312f45209c6b67231a3533a3f908857f4eaad
JiachenLiu09 <12990659@student.uts.edu.au>
2019-10-16 21:06:40 +1100

    finish

--- app.js
+++ app.js
@@ -26,16 +26,7 @@ db.connect((err) => {
 });
 
 app.get('/workshops', urlencodedParser, function(req, res) {
-    let skillSetId = req.body.skillSetId;
-    let sql = `SELECT * FROM workShop WHERE skillSetId=${skillSetId};`;
-    let query = db.query(sql, function(err, result) {
-        if(err) console.log(err);
-        res.end(JSON.stringify(result, null, 2));
-    });
-});
-
-app.get('/skillSets', urlencodedParser, function(req, res) {
-    let sql = `SELECT * FROM skillSet;`;
+    let sql = `SELECT * FROM workShop;`;
     let query = db.query(sql, function(err, result) {
         if(err) console.log(err);
         res.end(JSON.stringify(result, null, 2));
